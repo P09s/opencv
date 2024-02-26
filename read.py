17f325033d0d9eef362a75b1ab23d2b065da5d57
Parag Sharma <sharmaparag2004@gmail.com>
2024-02-26 21:14:53 +0530

    video capture

--- read.py
+++ read.py
@@ -1,9 +1,23 @@
 import cv2 as cv
 
-# read image
-img = cv.imread('Photos/2023-03-05.png')
+# # read image
+# img = cv.imread('Photos/2023-03-05.png')
 
-# display image
-cv.imshow('2023-03-05', img)
+# # display image
+# cv.imshow('2023-03-05', img)
 
-cv.waitKey(0)
+# cv.waitKey(0)
+
+# read videos 
+capture = cv.VideoCapture('videos/ar filter demo.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+
+    cv.imshow('videos', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
