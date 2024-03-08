2b022cd0b627153b5019017a4d0ec8110248b213
Parag Sharma <sharmaparag2004@gmail.com>
2024-03-08 12:26:22 +0530

    update

--- contours.py
+++ contours.py
@@ -6,13 +6,16 @@ cv.imshow('bro', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('gray', gray)
 
-blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
-cv.imshow('blur', blur)
+# blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
+# cv.imshow('blur', blur)
 
-canny = cv.Canny(blur, 125, 125)
-cv.imshow('canny', canny)
+# canny = cv.Canny(blur, 125, 125)
+# cv.imshow('canny', canny)
 
-contour, hierarchy = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+ret, thresh = cv.threshold(gray, 125, 225, cv.THRESH_BINARY)
+cv.imshow('thresh', thresh)
+
+contour, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 print(f'{len(contour)} contour(s) found !' )
 
 cv.waitKey(0)
