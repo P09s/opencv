23b2261944df4bf46ff3e7e39ac80778e070a48e
Parag Sharma <sharmaparag2004@gmail.com>
2024-03-09 17:47:47 +0530

    update

--- Advance concpets/smoothing.py
+++ Advance concpets/smoothing.py
@@ -4,7 +4,11 @@ img = cv.imread('Photos/bro.jpg')
 cv.imshow('bro', img)
 
 # averaging in image
-avg = cv.blur(img, (3,3))
+avg = cv.blur(img, (7,7))
 cv.imshow('average', avg)
+
+# gaussian blur (it is more natural looking as compare to the blur by using average)
+gauss = cv.GaussianBlur(img, (7,7), 0)
+cv.imshow('gauss', gauss)
 
 cv.waitKey(0)
