34997e261509588a6a84cdd36d719c133c6cb38f
Parag Sharma <sharmaparag2004@gmail.com>
2024-03-09 18:14:14 +0530

    update

--- Advance concpets/smoothing.py
+++ Advance concpets/smoothing.py
@@ -11,6 +11,12 @@ cv.imshow('average', avg)
 gauss = cv.GaussianBlur(img, (7,7), 0)
 cv.imshow('gauss', gauss)
 
+# median blurrring
 median = cv.medianBlur(img, 7)
 cv.imshow('medain', median)
+
+# bilateral blurring (most effective and used in mutltiple advance CV project cause it retains the the edges and dosen't remove them)
+bilateral = cv.bilateralFilter(img, 7, 15, 15)
+cv.imshow('bilateral', bilateral)
+
 cv.waitKey(0)
