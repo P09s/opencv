0e174cc82ec9d85e29bf36453cba114ba7eb5e5f
Parag Sharma <sharmaparag2004@gmail.com>
2024-03-09 22:18:03 +0530

    update

--- Advance concpets/histogram.py
+++ Advance concpets/histogram.py
@@ -4,16 +4,32 @@ import matplotlib.pyplot as plt
 img = cv.imread('Photos/bro.jpg')
 cv.imshow('bro', img)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('gray', gray)
+## Grayscale histogram
 
-gray_hist = cv.calcHist([gray], [0], None, [256], [0,256])
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('gray', gray)
+
+# gray_hist = cv.calcHist([gray], [0], None, [256], [0,256])
+
+# plt.figure()
+# plt.title('Grayscale histograms')
+# plt.xlabel('bins')
+# plt.ylabel('# of pixels')
+# plt.plot(gray_hist)
+# plt.show()
+
+## colored histogram
 
 plt.figure()
-plt.title('Grayscale histograms')
+plt.title('Colored histograms')
 plt.xlabel('bins')
 plt.ylabel('# of pixels')
-plt.plot(gray_hist)
+
+color = ['b', 'g', 'r']
+for i, col in enumerate(color):
+    hist = cv.calcHist([img], [i], None, [256], [0,256])
+    plt.plot(hist, color=col)
+    plt.xlim([0, 256])
 plt.show()
 
 cv.waitKey(0)
