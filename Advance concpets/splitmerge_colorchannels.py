657507c15a6261239d590c89485b9e5ee8e464b5
Parag Sharma <sharmaparag2004@gmail.com>
2024-03-09 17:21:25 +0530

    update

--- Advance concpets/splitmerge_colorchannels.py
+++ Advance concpets/splitmerge_colorchannels.py
@@ -3,4 +3,14 @@ import cv2 as cv
 img = cv.imread('Photos/bro.jpg')
 cv.imshow('img', img)
 
+b, g, r = cv.split(img)
+cv.imshow('blue', b)
+cv.imshow('green', g)
+cv.imshow('red', r)
+
+print(img.shape)
+print(b.shape)
+print(g.shape)
+print(r.shape)
+
 cv.waitKey(0)
