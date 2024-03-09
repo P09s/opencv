9ef6b3a3e43ddfce14944d6aa2182219b66f9868
Parag Sharma <sharmaparag2004@gmail.com>
2024-03-09 23:17:13 +0530

    update

--- Advance concpets/edge_detection.py
+++ Advance concpets/edge_detection.py
@@ -13,5 +13,16 @@ lap = np.uint8(np.absolute(lap))
 cv.imshow('lap', lap)
 
 # sobel
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+combined = cv.bitwise_or(sobelx, sobely)
+
+# canny is more advance algorithm compares to sobel
+canny = cv.Canny(gray, 150, 175)
+
+cv.imshow('soble x', sobelx)
+cv.imshow('sobel y', sobely)
+cv.imshow('combined', combined)
+cv.imshow('canny', canny)
 
 cv.waitKey(0)
